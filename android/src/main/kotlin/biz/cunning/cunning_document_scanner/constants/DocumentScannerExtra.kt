0179b1c1970dc2f387352d46c93763a4cceda732
MaduraDiasTreinetic <102131928+MaduraDiasTreinetic@users.noreply.github.com>
2023-02-21 12:18:31 +0530

    Delete java_pid45463.hprof

---
+++ android/src/main/kotlin/biz/cunning/cunning_document_scanner/constants/DocumentScannerExtra.kt
@@ -0,0 +1,11 @@
+package biz.cunning.cunning_document_scanner.constants
+
+/**
+ * This class contains constants meant to be used as intent extras
+ */
+class DocumentScannerExtra {
+    companion object {
+        const val EXTRA_LET_USER_ADJUST_CROP = "letUserAdjustCrop"
+        const val EXTRA_MAX_NUM_DOCUMENTS = "maxNumDocuments"
+    }
+}
